e777143116f056131aa92c9e6694dcaadce8898a
Cihan Tekin <cihantekin1990@hotmail.com.tr>
2018-11-28 23:11:55 +0300

    changes

--- marioplan/src/App.js
+++ marioplan/src/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import Navbar from './components/layout/Navbar'
+import Dashboard from './components/dashboard/Dashboard';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <div className="App">
-          <h1>MarioPlan</h1>
+          <Navbar />
+          <Switch>
+            <Route path="/" component={Dashboard}></Route>
+          </Switch>
         </div>
       </BrowserRouter>
 
---
+++ marioplan/src/components/projects/ProjectSummary.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const ProjectSummary = () => {
+    return(
+            <div className="card z-depth-0 project-summary">
+                <div className="card-content grey-text text-darken-3">
+                    <span className="card-title">Project Title</span>
+                    <p>Posted By CT !</p>
+                    <p className="grey-text">28 November, 22 am</p>
+                </div>
+            </div>
+    )
+}
+
+export default ProjectSummary
