0c4b1b04dc7ea2b578b8fd1a61ab5c454c59ca62
Cihan Tekin <cihantekin1990@hotmail.com.tr>
2018-11-30 17:45:30 +0300

    changes

--- marioplan/src/components/dashboard/Dashboard.js
+++ marioplan/src/components/dashboard/Dashboard.js
@@ -1,13 +1,17 @@
 import React, {Component} from 'react';
 import ProjectList from '../projects/ProjectList'
 import Notifications from './Notifications'
+import {connect} from 'react-redux'
 
 class Dashboard extends Component {
+
     render(){
+        const {projects} = this.props.projects;
+        console.log(this.props);
         return(
             <div className="dashboard container">
                 <div className="row">
-                    <div className="col s12 m6"><ProjectList/></div>
+                    <div className="col s12 m6"><ProjectList projects={projects}/></div>
                     <div className="col s12 m5 offset-1"><Notifications/></div>
                 </div>
             </div>
@@ -15,4 +19,10 @@ class Dashboard extends Component {
     }
 } 
 
-export default Dashboard 
+const mapStateToProps = (state) => {
+    return{
+        projects: state.project.projects
+    }
+}
+
+export default connect(mapStateToProps)(Dashboard) 
--- marioplan/src/store/reducers/projectReducer.js
+++ marioplan/src/store/reducers/projectReducer.js
@@ -1,5 +1,9 @@
 const initState = {
-
+    projects: [
+        {id: 1, title: "Spora Git !", content: "Göğüs çalışılacak."},
+        {id: 2, title: "Kitap Oku !", content: "Yeni kitaba başlanacak"},
+        {id: 3, title: "Sinemaya Git !", content: "bla bla bllaaa."},
+    ]
 }
 
 const projectReducer = (state = initState, action) => {
